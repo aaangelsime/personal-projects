485db809a8811976e586c3090919e23e387bc08e
Angel Simental <aasimental@icloud.com>
2025-12-19 11:16:43 -0700

    working on raw mode still

--- text-editor/main.c
+++ text-editor/main.c
@@ -1,86 +1,226 @@
-#include <unistd.h> // POSIX OS API (read, STDIN_FILENO)
-#include <errno.h> // System error numbers
-#include <termios.h> // Terminal I/O interface
-#include <stdlib.h> // Standard lib (exit, atexit)
-#include <ctype.h> // Character classification (iscntrl)
-#include <stdio.h> // Standard I/O (printf, perror)
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
 
-//functions
-void enable_raw_mode();
-void disable_raw_mode();
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** DEFINES ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
+
+/*** STRUCTS ***/
+
+struct editorConfig {
+    int cx, cy;
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
+
+/*** FUNCTION PROTOTYPES ***/
+
+void enableRawMode();
+void disableRawMode();
 void die(const char *s);
 
-//structs
-struct termios orig_termios; // holds original terminal state to restore later
+int editorReadKey();
+void editorProcessKeypress();
+void editorMoveCursor(int key);
 
-int main ()
-{
-    enable_raw_mode();
+void editorRefreshScreen();
+void editorDrawRows();
+void editorClearScreen();
 
-    while (1)
-    {
-        char c = '\0';
-        // byte read from stdin; returns -1 on error
-        // EAGAIN check is for non blocking timeouts
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+int getWindowSize(int *rows, int *cols);
+void initEditor();
 
-        // if char is control char (Ctrl, Esc, Backspace)
-        if(iscntrl(c))
-        {
-            //printf ; can print mult. representations of a byte.
-            //&d tells it to format the byte as decimal num
-            printf("%d\r\n", c); 
-        }
-        else 
-        {
-            //%c tells it to write out the byte directly as a char 
-            printf("%d ('%c')\r\n", c, c);
-        }
+/*** MAIN ***/
 
-        if (c== 'q') break;
+int main() {
+    enableRawMode();
+    initEditor();
+
+    while (1) {
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
 
-//en fn
-void enable_raw_mode()
-{
-    // get current terminal attributes
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+/*** TERMINAL ***/
 
-    //register cleanup handler to restore terminal on exit
-    atexit(disable_raw_mode);
-
-    struct termios raw = orig_termios;
-
-    //bitwire not/and to disable specific terminal flags:
-    // ICRNL: Fixes Ctrl-M; OPOST: turns off output processing
-    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag &= ~(CS8); // set char size to 8 bits
-
-    // ECHO
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-
-    raw.c_cc[VTIME] = 1;
-    raw.c_cc[VMIN] = 0;
-
-
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+void die(const char *s) {
+    editorClearScreen();
+    perror(s);
+    exit(1);
 }
 
-//dis fn
-void disable_raw_mode()
-{
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-    {
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios);
+}
+
+void enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
+        die("tcgetattr");
+
+    atexit(disableRawMode);
+
+    struct termios raw = E.orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
         die("tcsetattr");
+}
+
+int editorReadKey() {
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+            }
+        }
+        return '\x1b';
+    }
+
+    return c;
+}
+
+/*** OUTPUT ***/
+
+void editorClearScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorDrawRows() {
+    for (int y = 0; y < E.screenrows; y++) {
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int len = snprintf(welcome, sizeof(welcome),
+                "Mini Editor -- Ctrl-Q to quit");
+
+            if (len > E.screencols) len = E.screencols;
+
+            int padding = (E.screencols - len) / 2;
+            if (padding) {
+                write(STDOUT_FILENO, "~", 1);
+                padding--;
+            }
+            while (padding--) write(STDOUT_FILENO, " ", 1);
+            write(STDOUT_FILENO, welcome, len);
+        } else {
+            write(STDOUT_FILENO, "~", 1);
+        }
+
+        write(STDOUT_FILENO, "\x1b[K", 3);
+        write(STDOUT_FILENO, "\r\n", 2);
     }
 }
 
-//error handling fn
-void die(const char *s)
-{
-    perror(s);
-    exit(1);
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[?25l", 6);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    write(STDOUT_FILENO, buf, strlen(buf));
+
+    write(STDOUT_FILENO, "\x1b[?25h", 6);
+}
+
+/*** INPUT ***/
+
+void editorMoveCursor(int key) {
+    switch (key) {
+        case ARROW_LEFT:
+            if (E.cx > 0) E.cx--;
+            break;
+        case ARROW_RIGHT:
+            if (E.cx < E.screencols - 1) E.cx++;
+            break;
+        case ARROW_UP:
+            if (E.cy > 0) E.cy--;
+            break;
+        case ARROW_DOWN:
+            if (E.cy < E.screenrows - 1) E.cy++;
+            break;
+    }
+}
+
+void editorProcessKeypress() {
+    int c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+            editorClearScreen();
+            exit(0);
+            break;
+
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
+    }
+}
+
+/*** INIT ***/
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+        return -1;
+
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+}
+
+void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
 }

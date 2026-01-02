d026bf31a440fdbb1837a0865cf7a17018f4dddd
Angel Simental <aasimental@icloud.com>
2026-01-01 21:34:59 -0700

    Restarting 30dop

---
+++ main_projects/30-days-py/day_one.py
@@ -0,0 +1,32 @@
+### NOTE(s)
+"""
+    Indentation - Is used to create blocks of code, like curly brackets in c
+
+    Lists - is an ordered collection which allows to store different data types,
+            similiar to an array
+
+            Example
+          - [0,1,2,3] a list of numbers
+          - ['banana', 10, false, 9,18] different data types in a list; string, int, bool, float
+
+    Dictionary - Is an unordered collection of data in a key value pair format
+
+            Example
+            {
+            'first_name':'Angel',
+            'last_name':'Simental',
+            'age':25,
+            'is_married':True
+            'skills':['JS', 'C++', 'C', 'Python']
+            }
+
+    Tuple - An ordered collection of data like list but cannot be modified
+
+            Example
+          - ('Angel', 'Sarah', 'Lucas', 'Arya') Names
+
+    Set - Collection of data types, unlike list & tuple, set is not an ordered collection of items
+
+            Example
+          - {2.14, 9.74, 1.89} order is not important in a set
+"""

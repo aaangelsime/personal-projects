f28c2ecdaa7e9fed7908e3ca284bfe85800231f9
Angel Simental <aasimental@icloud.com>
2026-01-02 21:25:30 -0700

    finish Exercise for day 2

---
+++ software_projects/30-days-py/day_two/day_two.py
@@ -0,0 +1,67 @@
+# BUILT IN FUNCTIONS & VARIABLES
+
+"""
+Commonly used Built In Functions:
+    print() writes object
+    len() returns the length of an object
+    type() returns the type of an object 
+    input() user input
+    min() returns the smallest item of two or more arguments
+    max() returns the largest item of two or more arguments
+    sorted() return a new sorted list from the items in iterable
+    open() open a file a return corresponding file object
+"""
+
+# prints min 
+
+"""
+print(min(20, 30, 40, 60, 100))
+"""
+
+# Dictionary variable with print and type
+"""
+person_info = {
+    'firstname':'Angel',
+    'lastname':'Simental',
+    'age':25,
+    'state':'Colorado'
+}
+print(person_info)
+print(type(person_info))
+"""
+
+# Declaring Multiple Variable in a line
+"""
+EXAMPLE:
+
+first_name, last_name, state, age, is_married = 'Angel', 'Simental', 'Colorado', '24', True
+print(first_name, last_name, state, age, is_married)
+
+"""
+
+# Getting user Input
+"""
+first_name = input('What is your name: ')
+age = input('What is your age: ')
+
+print('First Name:', first_name)
+print('Age:', age)
+"""
+
+###CASTING
+"""
+Casting = Converting one data type to another data type.
+
+# string to list conversion
+city = 'Grand Valley'
+print('String:', city)
+city_to_list = list(city)
+print('List:', city_to_list)
+"""
+
+#EXERCISE
+
+
+
+
+

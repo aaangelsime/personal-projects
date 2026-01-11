b565468b1a3b9de75c38c014027b95920a709a8e
Angel <aasimental@icloud.com>
2026-01-10 18:10:05 -0700

    updates

---
+++ embedded_lowlevel_projects/practice-fundamentals/c/Pointers/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+To run without makefile 
+    gcc main.c -o myprogram
+    ./myprogram
+
+To run with make file
+make makerun
+or 
+make && make makerun
+*/
+
+
+int main(void)
+// argc = number of command line arguments
+// argv = list of command-line arguments; more of an argument vector, or pointer to an array of char-string arguments
+{
+    int x = 10;
+    int *p = &x; // p stores the address of x
+
+    // format specifier %vartype
+    printf("x = %d\n", x); // value of x
+    printf("x memory address = %p\n", (void*)&x); // address of x
+    printf("*p = %d\n", *p); // value at address
+    printf("p = %p\n", (void*)p); // address of x
+    printf("&p = %p\n", (void*)&p); // address of pointer variable
+    
+    return 0;
+
+}

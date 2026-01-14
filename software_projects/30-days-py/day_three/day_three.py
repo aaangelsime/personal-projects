abe9ecd265c94af8b7e631ecb110be815c5c403b
Angel <aasimental@icloud.com>
2026-01-13 20:36:44 -0700

    updates

--- software_projects/30-days-py/day_three/day_three.py
+++ software_projects/30-days-py/day_three/day_three.py
@@ -22,7 +22,31 @@ x = complex(1 + 5j)
 print('Age:', age)
 print('Age Variable type:', type(age))
 
-print('')
+x = int(input('Enter value of base x: '))
+y = int(input('Enter value of height y: '))
+triangle_area = 0.5 * x * y
+print('Triangle Area =', triangle_area)
+
+a = int(input('Enter side a: '))
+b = int(input('Enter side b: '))
+c = int(input('Enter side c: '))
+triangle_perimeter = a + b + c
+print('The perimeter of the triangle is', triangle_perimeter)
+
+length = int(input('Enter length of rectangle: '))
+width = int(input('Enter width of rectangle: '))
+rectangle_perimeter = 2 * (length + width)
+print('Rectangle perimeter =', rectangle_perimeter)
+
+
+u = 'python'
+i = 'dragon' 
+
+print(len(u))
+print(len(i))
+print('u length == i length :', len(i) == len(u))
+print('on is in dragon:', 'on' in 'dragon')
+print('on is in python:', 'on' in 'python')
 
 
 

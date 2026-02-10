a6d621511fc7c309d6170149e8ca3538515df755
Angel Simental <aasimental@icloud.com>
2026-02-09 22:37:46 -0700

    classes refresher

---
+++ low_level/practice-fundamentals/classes/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+class vehicle
+{
+        int _num_tires, _num_doors;
+    public:
+        void set_val(int, int);
+        int get_tires(), get_doors();
+};
+
+void vehicle::set_val(int num_tires, int num_doors)
+{
+    _num_doors = num_doors;
+    _num_tires = num_tires;
+}
+int vehicle::get_doors() { return _num_doors; }
+int vehicle::get_tires() { return _num_tires; }
+
+
+
+class Truck : vehicle
+{
+     std::string _bed_type;
+     std::string _fuel_type;
+    public:
+        void set_val(std::string, std::string);
+        std::string get_bed(), get_fuel();
+};
+
+void Truck::set_val(std::string bed_type, std::string fuel_type)
+{
+    _bed_type = bed_type;
+    _fuel_type = fuel_type;
+}
+std::string Truck::get_bed() { return _bed_type; } 
+std::string Truck::get_fuel() { return _fuel_type; }
+
+
+
+int main (int argc, char* argv[])
+{
+    vehicle v1;
+    int door, tires;
+
+    Truck small_truck;
+    small_truck.set_val("Flat Bed", "Desiel");
+    std::string bed, fuel;
+
+    std::cout << "Enter the amount of doors in your vehicle: " ;
+    std::cin >> door;
+    std::cout << "Enter the amount of tires in your vehicle: " ;
+    std::cin >> tires;
+
+    v1.set_val(door, tires);
+
+    std::cout << "Enter the amount of doors in your vehicle: " ;
+    std::cin >> door;
+    std::cout << "Enter the amount of tires in your vehicle: " ;
+    std::cin >> tires;
+
+    // std::cout << "Number of doors: " << v1.get_doors() << std::endl;
+    // std::cout << "Number of tires: " << v1.get_tires() << std::endl;
+
+    // std::cout << "Bed type: " << small_truck.get_bed() << std::endl;
+    // std::cout << "Fuel type: " << small_truck.get_fuel() << std::endl;
+
+    return 0;
+}

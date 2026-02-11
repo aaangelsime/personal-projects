4704705db82ffc586934c801905549530b1fde88
Angel <aasimental@icloud.com>
2026-02-11 07:54:15 -0700

    refreshing on pass by refrence

--- low_level/practice-fundamentals/pass_by_reference/main.cpp
+++ low_level/practice-fundamentals/pass_by_reference/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+void swap(int &x, int &y);
+
 int main(int argc, char* argv[])
 {
+    int x = 10;
+    int y = 5;
+
+    swap(x, y);
+
+    std::cout << "&x: " << &x << std::endl;
+    std::cout << "&y; " << &y << std::endl;
+
     return 0;
+}
+
+void swap(int &x, int &y)
+{
+    std::cout << "&x: " << &x << std::endl;
+    std::cout << "&y; " << &y << std::endl;
 }
